460dd9e186fa3df2602651005354e9517a2917c1
BoumeshouliYouness <boumeshouliyouness6@gmail.com>
2025-02-10 16:45:19 +0100

    database

---
+++ config/routes.php
@@ -0,0 +1,18 @@
+<?php
+// Routes pour les patients
+$router->add('GET', '/patients', 'PatientController', 'index');
+$router->add('GET', '/pateint_dashboard', 'PatientController', 'index');
+$router->add('GET', '/patients', 'PatientController', 'index');
+$router->add('GET', '/pateint_dashboard', 'PatientController', 'index');
+$router->add('GET', '/patients', 'PatientController', 'index');
+$router->add('GET', '/pateint_dashboard', 'PatientController', 'index');
+$router->add('GET', '/patients', 'PatientController', 'index');
+$router->add('GET', '/pateint_dashboard', 'PatientController', 'index');
+$router->add('GET', '/patients', 'PatientController', 'index');
+$router->add('GET', '/pateint_dashboard', 'PatientController', 'index');
+$router->add('GET', '/patients', 'PatientController', 'index');
+$router->add('GET', '/pateint_dashboard', 'PatientController', 'index');
+$router->add('GET', '/patients', 'PatientController', 'index');
+$router->add('GET', '/pateint_dashboard', 'PatientController', 'index');
+
+
---
+++ core/Database.php
@@ -0,0 +1,40 @@
+<?php
+namespace Core;
+
+$config = require_once 'C:\laragon\www\SARI3\config\config.php';
+
+
+
+class Database {
+    private static $instance = null;
+    private $connection;
+
+    private function __construct() {
+        global $config; 
+        $host = "localhost";
+        $port = "5432";
+        $dbname = "sarii";
+        $user = "postgres";
+        $pass = "Youness";
+
+        try {
+            $this->conn = new \PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
+            var_dump($this->conn);
+        } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            echo 'work';
+            self::$instance = new self();
+        }
+        var_dump(self::$instance);
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
