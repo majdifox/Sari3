f0590b6a821f3f9a78a79f1504ce4ce1457642b0
BoumeshouliYouness <boumeshouliyouness6@gmail.com>
2025-02-11 10:37:39 +0100

    Routes

--- config/routes.php
+++ config/routes.php
@@ -1,18 +1,16 @@
 <?php
-// Routes pour les patients
-$router->add('GET', '/patients', 'PatientController', 'index');
-$router->add('GET', '/pateint_dashboard', 'PatientController', 'index');
-$router->add('GET', '/patients', 'PatientController', 'index');
-$router->add('GET', '/pateint_dashboard', 'PatientController', 'index');
-$router->add('GET', '/patients', 'PatientController', 'index');
-$router->add('GET', '/pateint_dashboard', 'PatientController', 'index');
-$router->add('GET', '/patients', 'PatientController', 'index');
-$router->add('GET', '/pateint_dashboard', 'PatientController', 'index');
-$router->add('GET', '/patients', 'PatientController', 'index');
-$router->add('GET', '/pateint_dashboard', 'PatientController', 'index');
-$router->add('GET', '/patients', 'PatientController', 'index');
-$router->add('GET', '/pateint_dashboard', 'PatientController', 'index');
-$router->add('GET', '/patients', 'PatientController', 'index');
-$router->add('GET', '/pateint_dashboard', 'PatientController', 'index');
+
+$router->add('GET', '/admin', 'AdminController', 'dashboard');
+$router->add('GET', '/admin/utilisateurs/ban/{id}', 'adminController', 'index');
+$router->add('GET', '/admin/inetiare/refuse/{id}', 'adminController', 'refuseInetieare');
+$router->add('GET', '/admin/inetiare/accept/{id}', 'adminController', 'acceptInetieare');
+$router->add('GET', '/expoditeur', 'expoditeurController', 'index');
+$router->add('GET', '/expoditeur/AccountVerification', 'expoditeurController', 'verification');
+$router->add('GET', '/expoditeur/IneatieareDemande/{@}', 'expoditeurController', 'sendRequest');
+$router->add('GET', '/login', 'authController', 'login');
+$router->add('GET', '/logout', 'authController', 'logout');
+$router->add('GET', '/conducteur/IneatieareAnnoncement', 'conducteurController', 'annoncement');
+$router->add('GET', '/conducteur/IneatieareDeletion', 'conducteurController', 'DeleteIneatieare');
+
 
 
---
+++ core/Model.php
@@ -0,0 +1,8 @@
+<?php
+
+interface Model {
+   public function create();
+   public function delete();
+   public function update();
+   public function read();
+}
