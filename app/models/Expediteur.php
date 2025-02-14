def0c2294cf49d24a4455b22736419841f33e1ba
BoumeshouliYouness <boumeshouliyouness6@gmail.com>
2025-02-14 12:33:42 +0000

    fix page check annonce

--- app/models/Expediteur.php
+++ app/models/Expediteur.php
@@ -9,15 +9,13 @@ use App\Models\ItineraireFactory;
 
 class Expediteur extends  User {
     private $ItineraireFactory;
-    private $ColisFactory;
-    private $UserFactory;
     
 
     public function __construct($id = null ,$cnie = null,$nom = null  ,$prenom = null,$email = null,$role = null,$datecreation = null){
       parent::__construct();
       
       $this->id =  $id;
-      $this->cnie =  $cnie;
+      $this->cni =  $cnie;
       $this->nom =  $nom;
       $this->prenom =  $prenom;
       $this->email =  $email;
@@ -27,10 +25,16 @@ class Expediteur extends  User {
       $this->ColisFactory = new ColisFactory;
       $this->UserFactory = new UserFactory;
   }
-    public function MakeRequest($data) {
-    // data fiha id dyl itineraire o data dyl colis dyl expediteur
-    // ghadi n3mro db table dyl Colis
+  public function getItinerairebyID($id){
+    return $Itineraire =  $this->ItineraireFactory->getItineraire($id);
+   
+  }
+  public function createItiniraireDetails(Itineraire $Itineraire){
+    return $details =  $this->ItineraireFactory->createItiniraireDetails($Itineraire);
     
+   }
+    public function MakeRequest($data) {
+    $this->ColisFactory->addColis($data);
   }
 
   public function getProfileInfos($id_expediteur){
@@ -39,7 +43,7 @@ class Expediteur extends  User {
 
   public function deleteColis($idColis) {
     //  search itineraire by idColis and check if status still en preparation and delete it from the table colis
-    $colis = $this->ColisFactory->getColis($idColis);
+    $colis = $this->colisFactory->getColis($idColis);
   }
   public function showIniteraire() {
     //  search itineraire by Exp 
