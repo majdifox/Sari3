aade768dad6f385eb728c64f63b686b1570cf74a
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-02-14 15:50:43 +0100

    Updated the form

--- app/views/expediteur/Profile_Expediteur.php
+++ app/views/expediteur/Profile_Expediteur.php
@@ -76,10 +76,6 @@
                     </div>
                     <div class="grid lg:grid-cols-2 gap-6">
                         <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Password</label>
-                            <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        </div>
-                        <div>
                             <label for="photo_url" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Photo URL</label>
                             <input type="text" name="photo_url" id="photo_url" value="<?= htmlspecialchars($user['photo'] ?? '') ?>" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         </div>
